0edd56c7bcf0d7046ef03b0ce958b7519008b682
home <dark9121@gmail.com>
2025-11-23 18:57:27 +0900

    provider 버전 수정 및 테스트 계정 생성

--- terraform/modules/aft-account-request/versions.tf
+++ terraform/modules/aft-account-request/versions.tf
@@ -2,12 +2,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 terraform {
-  required_version = ">= 1.0.0"
+  #required_version = ">= 1.0.0"
+  required_version = ">= 0.15.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.11.0, < 6.0.0"
+      #version = ">= 5.11.0, < 6.0.0"
+      version = ">= 3.15"
     }
   }
 }
